b2f8889541c8ec37357d84ded3d783ab25d83625
Najeeb Ahmad <105259193+NajeebAhmad00@users.noreply.github.com>
2024-04-17 13:07:08 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: procss.env.REACT_APP_API_KEY,
-    authDomain: procss.env.REACT_APP_AUTH_DOMAIN,
-    projectId: procss.env.REACT_APP_PROJECT_ID,
-    storageBucket: procss.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: procss.env.REACT_MESSAGING_SENDER_ID,
-    appId: procss.env.REACT_APP_API_ID
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_API_ID
 }
 
 const app = initializeApp(firebaseConfig)
